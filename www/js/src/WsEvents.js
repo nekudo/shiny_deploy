ce92ce054577bef41df1bb0056db2d2d7019d5d6
Simon Samtleben <simon@samtleben.me>
2015-03-27 20:22:46 +0100

    Apply minor improvements

--- www/js/src/WsEvents.js
+++ www/js/src/WsEvents.js
@@ -13,19 +13,16 @@ function WsEvents() {
         var elLog = $('#log');
         var logClass = 'log-' + type;
         var msgLines = message.split("\n");
-        if (msgLines.length > 1) {
-            msgLines.reverse();
-        }
         $.each(msgLines, function(i, msgLine) {
             var logMsg = '<div class="logMsg"><span class="log-time">' + time
                 + '</span> <span class="log-source">' + source
                 + '</span> <span class="' + logClass + '">' + msgLine + '</span></div>';
             var elLogMsg = $(logMsg);
-            elLog.prepend(elLogMsg);
+            elLog.append(elLogMsg);
         });
         while ($('.logMsg').length > 2000) {
-            elLog.find('logMsg').last().remove();
+            elLog.find('logMsg').first().remove();
         }
-        $(".nano").nanoScroller();
+        $(".nano").nanoScroller().nanoScroller({ scroll: 'bottom' });
     }
 }
