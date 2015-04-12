c03a68f2553f37a74af99542a762356a0e31de96
Simon Samtleben <simon@samtleben.me>
2015-04-12 10:23:46 +0200

    Cleanup angular websocket provider

--- www/js/app/services/servers.js
+++ www/js/app/services/servers.js
@@ -1,5 +1,5 @@
 app.service('serversService', function (ws) {
     this.getServers = function () {
-        return ws.sendCallbackRequest('getServers');
+        return ws.sendDataRequest('getServers');
     };
 });
