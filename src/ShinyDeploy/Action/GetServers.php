f082c7ed0cd5f370d33986043f8ba3d186e6d913
Simon Samtleben <simon@nekudo.com>
2015-04-12 22:35:12 +0200

    Add websocket data responder

--- src/ShinyDeploy/Action/GetServers.php
+++ src/ShinyDeploy/Action/GetServers.php
@@ -1,15 +1,24 @@
 <?php
 namespace ShinyDeploy\Action;
 
-use ShinyDeploy\Core\Action;
 use ShinyDeploy\Domain\Servers;
+use ShinyDeploy\Responder\WsDataResponder;
 
-class GetServers extends Action
+class GetServers extends WsDataAction
 {
-    public function __invoke()
+    public function __invoke($actionPayload)
     {
         $serversDomain = new Servers($this->config, $this->logger);
         $servers = $serversDomain->getServers();
+
+        $responder = new WsDataResponder($this->config, $this->logger);
+        $this->setResponse($responder);
+
         return $servers;
+    }
+
+    public function setResponse(WsDataResponder $responder)
+    {
+        $this->responder = $responder;
     }
 }
---
+++ src/ShinyDeploy/Action/WsDataAction.php
@@ -0,0 +1,21 @@
+<?php
+namespace ShinyDeploy\Action;
+
+use ShinyDeploy\Core\Action;
+use ShinyDeploy\Responder\WsDataResponder;
+
+abstract class WsDataAction extends Action
+{
+    /** @var WsDataResponder $responder */
+    protected $responder;
+
+    abstract public function __invoke($actionPayload);
+
+    abstract public function setResponse(WsDataResponder $responder);
+
+    public function getResponse($callbackId)
+    {
+        $this->responder->setCallbackId($callbackId);
+        return $this->$responder;
+    }
+}
