6295e4c4b13e3b37e319edd7abc21f67782577d6
Simon Samtleben <simon@nekudo.com>
2015-04-13 22:05:32 +0200

    Add alert-msg directive

--- www/js/app/controllers/servers.js
+++ www/js/app/controllers/servers.js
@@ -20,13 +20,30 @@ app.controller('ServersController', function ($scope, serversService) {
     };
 });
 
-app.controller('ServersAddController', function ($scope, $location, serversService) {
+app.controller('ServersAddController', function ($scope, $rootScope, $location, serversService) {
     $scope.addCustomer = function() {
         var promise = serversService.addServer($scope.server);
         promise.then(function(data) {
             $location.path('/servers');
         }, function(reason) {
-            console.log(reason);
+            $rootScope.$emit('message-event', reason);
         })
     }
 });
+
+
+// @todo Move to directives js file.
+app.directive("alertMsg", ['$rootScope', function ($rootScope) {
+    return {
+        restrict: "E",
+        scope: true,
+        template: '{{msg}}', // this string is the html that will be placed inside the <alert-msg></alert-msg> tags.
+        link: function ($scope, $element, attrs) {
+            var _unregister;
+            _unregister = $rootScope.$on('message-event', function (event, message) {
+                $scope.msg = message;
+            });
+            $scope.$on("$destroy", _unregister);
+        }
+    };
+}]);
