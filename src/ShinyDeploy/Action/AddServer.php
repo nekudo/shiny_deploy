4a1c2dc5a636385b38b390fbbf456b7295b8e18d
Simon Samtleben <simon@nekudo.com>
2015-04-17 19:24:29 +0200

    Add server side input validation

--- src/ShinyDeploy/Action/AddServer.php
+++ src/ShinyDeploy/Action/AddServer.php
@@ -4,25 +4,35 @@ namespace ShinyDeploy\Action;
 use ShinyDeploy\Domain\Servers;
 use ShinyDeploy\Exceptions\WebsocketException;
 use ShinyDeploy\Responder\WsDataResponder;
+use Valitron\Validator;
 
 class AddServer extends WsDataAction
 {
     public function __invoke($actionPayload)
     {
         $responder = new WsDataResponder($this->config, $this->logger);
+        $this->setResponse($responder);
         if (!isset($actionPayload['serverData'])) {
             throw new WebsocketException('Invalid addServer request received.');
         }
         $serverData = $actionPayload['serverData'];
-
-        // @todo Implement server side data validation!
-
         $serversDomain = new Servers($this->config, $this->logger);
+
+        // validate input:
+        $validator = new Validator($serverData);
+        $validator->rules($serversDomain->getCreateRules());
+        if (!$validator->validate()) {
+            $this->responder->setError('Input validation failed. Please check your data.');
+            return false;
+        }
+
+        // add server:
         $addResult = $serversDomain->addServer($serverData);
         if ($addResult === false) {
-            $responder->setError('testing error response...');
+            $this->responder->setError('Could not add server to database.');
+            return false;
         }
-        $this->setResponse($responder);
+        return true;
     }
 
     public function setResponse(WsDataResponder $responder)
--- src/ShinyDeploy/Domain/DatabaseDomain.php
+++ src/ShinyDeploy/Domain/DatabaseDomain.php
@@ -2,9 +2,11 @@
 namespace ShinyDeploy\Domain;
 
 use Apix\Log\Logger;
+use GG\Hostname;
 use Noodlehaus\Config;
 use ShinyDeploy\Core\Db;
 use ShinyDeploy\Core\Domain;
+use Valitron\Validator;
 
 class DatabaseDomain extends Domain
 {
@@ -12,10 +14,53 @@ class DatabaseDomain extends Domain
 
     public function __construct(Config $config, Logger $logger)
     {
+        // connect to database:
         $dbConfig = $config->get('db');
         if (empty($dbConfig)) {
             throw new \RuntimeException('Database configuration not set.');
         }
         $this->db = new Db($dbConfig['host'], $dbConfig['user'], $dbConfig['pass'], $dbConfig['db']);
+
+        // add custom validation rules:
+        $this->addCustomValidationRules();
+    }
+
+    /**
+     * Adds custom validation rules to validator lib.
+     */
+    private function addCustomValidationRules()
+    {
+        // add hostname validation:
+        Validator::addRule(
+            'hostname',
+            function ($field, $value, array $params) {
+                $realWorld = (isset($params[0]) && $params[0] === true) ? true : false;
+
+                // lets first transform it to ascii
+                $value = idn_to_ascii((string) $value);
+
+                // valid chars check
+                if (!preg_match('/^([a-z\d](-*[a-z\d])*)(\.([a-z\d](-*[a-z\d])*))*$/i', $value)) {
+                    return false;
+                }
+
+                // overall length check
+                if (!preg_match('/^.{1,253}$/', $value)) {
+                    return false;
+                }
+
+                // length of each label
+                if (!preg_match('/^[^\.]{1,63}(\.[^\.]{1,63})*$/', $value)) {
+                    return false;
+                }
+
+                if ($realWorld === true && !Hostname::valid($value)) {
+                    return false;
+                }
+
+                return true;
+            },
+            'not valid.'
+        );
     }
 }
