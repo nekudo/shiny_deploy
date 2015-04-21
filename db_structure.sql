632da0f383b3db4fd5052f324c49c54e9b788f1c
Simon Samtleben <simon@samtleben.me>
2015-04-21 20:03:41 +0200

    Add server/repositoy selects to deployment view

--- db_structure.sql
+++ db_structure.sql
@@ -47,3 +47,26 @@ ADD PRIMARY KEY (`id`);
 --
 ALTER TABLE `repositories`
 MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT;
+
+--
+-- Table structure for table `deployments`
+--
+CREATE TABLE IF NOT EXISTS `deployments` (
+  `id` int(10) unsigned NOT NULL,
+  `server_id` int(10) unsigned NOT NULL DEFAULT '0',
+  `repository_id` int(10) unsigned NOT NULL DEFAULT '0',
+  `branch` varchar(100) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `target_path` varchar(200) COLLATE utf8_unicode_ci NOT NULL DEFAULT ''
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Indexes for table `deployments`
+--
+ALTER TABLE `deployments`
+ADD PRIMARY KEY (`id`), ADD KEY `server_id` (`server_id`,`repository_id`);
+
+--
+-- AUTO_INCREMENT for table `deployments`
+--
+ALTER TABLE `deployments`
+MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT;
