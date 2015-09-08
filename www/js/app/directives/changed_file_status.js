f703f63c3c72e20a19d9b82cdbcb72c73f7c5516
Simon Samtleben <simon@samtleben.me>
2015-09-08 22:08:24 +0200

    Optimize directive

--- www/js/app/directives/changed_file_status.js
+++ www/js/app/directives/changed_file_status.js
@@ -6,42 +6,41 @@
 
     function changedFileStatus() {
 
-        return {
-            restrict: 'C',
+        var directive = {
+            restrict: 'A',
             template: '<span class="label {{ itemTypeClass }}">{{ itemTypeName }}</span>',
             scope: {
                 itemType: '=type'
             },
             replace: true,
-
-            link: function($scope, element, attributes) {
-                switch ($scope.itemType) {
-                    case 'A':
-                        $scope.itemTypeClass = 'label-success';
-                        $scope.itemTypeName = 'Added';
-                        break;
-                    case 'C':
-                        $scope.itemTypeClass = 'label-info';
-                        $scope.itemTypeName = 'Copied';
-                        break;
-                    case 'D':
-                        $scope.itemTypeClass = 'label-danger';
-                        $scope.itemTypeName = 'Deleted';
-                        break;
-                    case 'M':
-                        $scope.itemTypeClass = 'label-primary';
-                        $scope.itemTypeName = 'Modified';
-                        break;
-                    case 'R':
-                        $scope.itemTypeClass = 'label-info';
-                        $scope.itemTypeName = 'Renamed';
-                        break;
-
-                }
-                $scope.typeClass = ' label-primary';
-            }
+            link: link
         };
 
-    }
+        return directive;
 
+        function link(scope, element, attrs) {
+            switch (scope.itemType) {
+                case 'A':
+                    scope.itemTypeClass = 'label-success';
+                    scope.itemTypeName = 'Added';
+                    break;
+                case 'C':
+                    scope.itemTypeClass = 'label-info';
+                    scope.itemTypeName = 'Copied';
+                    break;
+                case 'D':
+                    scope.itemTypeClass = 'label-danger';
+                    scope.itemTypeName = 'Deleted';
+                    break;
+                case 'M':
+                    scope.itemTypeClass = 'label-primary';
+                    scope.itemTypeName = 'Modified';
+                    break;
+                case 'R':
+                    scope.itemTypeClass = 'label-info';
+                    scope.itemTypeName = 'Renamed';
+                    break;
+            }
+        }
+    }
 }());
