88077ae38fae66bad449eb41d62e8f6b2c1519f2
Simon Samtleben <simon@nekudo.com>
2015-10-11 12:09:23 +0200

    Clean some code in repository actions

--- src/ShinyDeploy/Action/CloneRepository.php
+++ src/ShinyDeploy/Action/CloneRepository.php
@@ -19,20 +19,28 @@ class CloneRepository extends Action
             if (empty($clientId)) {
                 throw new \RuntimeException('Client-ID can not be empty.');
             }
+
+            // Init responder:
             $notificationResponder = new WsNotificationResponder($this->config, $this->logger);
             $notificationResponder->setClientId($clientId);
+
+            // Init domains:
             $repositoriesDomain = new Repositories($this->config, $this->logger);
             $repositoryDomain = new Repository($this->config, $this->logger);
             $gitDomain = new Git($this->config, $this->logger);
+
+            // Get repo data.
             $repositoryData = $repositoriesDomain->getRepositoryData($repositoryId);
             if (empty($repositoryData)) {
                 $notificationResponder->send('Could not clone repository. Repository not found in database.', 'danger');
             }
 
+            // Check if repo is reachable:
             if ($repositoriesDomain->checkUrl($repositoryData) === false) {
                 $notificationResponder->send('Could not clone repository. URL not reachable.', 'warning');
             }
 
+            // Check if repo already exists:
             $repoPath = $repositoryDomain->createLocalPath($repositoryData['url']);
             if ($repositoryDomain->exists($repoPath) === true) {
                 $notificationResponder->send(
@@ -40,19 +48,23 @@ class CloneRepository extends Action
                     'success'
                 );
                 return true;
-            } else {
-                $repositoryUrl = $repositoriesDomain->getCredentialsUrl($repositoryData);
-                $response = $gitDomain->gitClone($repositoryUrl, $repoPath);
-                if (strpos($response, 'done.') !== false) {
-                    $notificationResponder->send(
-                        'Git clone of repository ' . $repositoryData['name'] . ' successfully completed.',
-                        'success'
-                    );
-                    return true;
-                }
             }
+
+            // Clone the repository:
+            $repositoryUrl = $repositoriesDomain->getCredentialsUrl($repositoryData);
+            $response = $gitDomain->gitClone($repositoryUrl, $repoPath);
+            if (strpos($response, 'done.') !== false) {
+                $notificationResponder->send(
+                    'Git clone of repository ' . $repositoryData['name'] . ' successfully completed.',
+                    'success'
+                );
+                return true;
+            }
+
+            // Someting went wrong, send error notice to client:
             $notificationResponder->send('Could not clone repository to local server.', 'danger');
             return false;
+
         } catch (\RuntimeException $e) {
             $this->logger->alert(
                 'Runtime Exception: ' . $e->getMessage() . ' (' . $e->getFile() . ': ' . $e->getLine() . ')'
--- src/ShinyDeploy/Action/DeleteRepositoryFiles.php
+++ src/ShinyDeploy/Action/DeleteRepositoryFiles.php
@@ -16,23 +16,31 @@ class DeleteRepositoryFiles extends Action
             if (empty($clientId)) {
                 throw new \RuntimeException('Client-ID can not be empty.');
             }
+
+            // Init responder:
             $notificationResponder = new WsNotificationResponder($this->config, $this->logger);
             $notificationResponder->setClientId($clientId);
 
+            // Init domains:
             $repositoryDomain = new Repository($this->config, $this->logger);
+
+            // Check if repo exists:
             if ($repositoryDomain->exists($repoPath) === false) {
                 $notificationResponder->send(
                     'Could not delete repository files. Local path not existing.',
                     'warning'
                 );
                 return true;
-            } else {
-                $response = $repositoryDomain->remove($repoPath);
-                if ($response === true) {
-                    $notificationResponder->send('Local repository files successfully deleted.', 'success');
-                    return true;
-                }
             }
+
+            // Delete repo files:
+            $response = $repositoryDomain->remove($repoPath);
+            if ($response === true) {
+                $notificationResponder->send('Local repository files successfully deleted.', 'success');
+                return true;
+            }
+
+            // Someting went wrong, respond with error message:
             $notificationResponder->send('Could not delete local repository files.', 'danger');
             return false;
 
