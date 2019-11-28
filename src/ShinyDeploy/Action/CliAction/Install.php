d74554e82bf4c07214435219c31c7930b2d99abf
Simon Samtleben <simon@samtleben.me>
2019-11-28 20:56:04 +0100

    Add system-user creation to installer

--- src/ShinyDeploy/Action/CliAction/Install.php
+++ src/ShinyDeploy/Action/CliAction/Install.php
@@ -3,6 +3,7 @@
 namespace ShinyDeploy\Action\CliAction;
 
 use ShinyDeploy\Action\CliAction;
+use ShinyDeploy\Domain\Database\Auth;
 use ShinyDeploy\Domain\Installer;
 
 class Install extends CliAction
@@ -15,6 +16,7 @@ class Install extends CliAction
     public function __invoke() : void
     {
         try {
+            $this->line('Testing connection to database...');
             $dbConfig = $this->config->get('db', null);
             if ($dbConfig === null || empty($dbConfig['host'])) {
                 $this->error('Error: Database config missing or invalid. Check your config file.');
@@ -25,15 +27,34 @@ class Install extends CliAction
                 $this->error('Error: Could not connect to MySQL database. Please check config file.');
             }
 
+            $this->line('Creating mysql tables...');
             if ($installer->createTables() === false) {
                 $this->error('Error: Could not create MySQL table. Check log for details.');
                 exit;
             }
 
-            // @todo create system user
+            $this->line('Creating system user...');
+            $this->info('ATTENTION: Please provide a strong password for your system account!');
+            $password = $this->requestUserInput('Password: ', true);
+            $passwordRepeat = $this->requestUserInput('Repeat: ', true);
+            if (empty($password)) {
+                $this->error('Error: System password can not be empty.');
+                exit;
+            }
+            if ($password !== $passwordRepeat) {
+                $this->error('Error: Passwords do not match.');
+                exit;
+            }
 
+            $auth = new Auth($this->config, $this->logger);
+            $result = $auth->createSystemUser($password);
+            if ($result === false) {
+                $this->error('Error: Could not create system user. Check log for details.');
+                exit;
+            }
 
             $this->success('Installation completed.');
+            $this->line('Tip: You should create a new user using the "user-add" command.');
         } catch (\Exception $e) {
             echo 'Error: ' . $e->getMessage() . PHP_EOL;
             echo 'File: ' . $e->getFile() . ':' . $e->getLine() . PHP_EOL;
